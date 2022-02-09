1c60ba8d26b320d89555c0c63e8edc4285249ac8
Eloisa-R <rios.rico.eloisa@gmail.com>
2022-02-09 13:58:34 +0100

    Added missing database name

--- server/utils/database.js
+++ server/utils/database.js
@@ -4,7 +4,7 @@ const logger = require('./logger');
 const { DB_USER_NAME, DB_PASSWORD } = process.env;
 
 const connectDatabase = async () => {
-  const databaseServerUrl = `mongodb+srv://${DB_USER_NAME}:${DB_PASSWORD}@cluster0.ipto4.mongodb.net/?retryWrites=true&w=majority`;
+  const databaseServerUrl = `mongodb+srv://${DB_USER_NAME}:${DB_PASSWORD}@cluster0.ipto4.mongodb.net/workflows?retryWrites=true&w=majority`;
   try {
     await mongoose.connect(databaseServerUrl);
   } catch (error) {
